fbffa3e1be6a0e27010d0f60dc2a03a57290d8ff
Kevin Gisi <kevin.gisi@calendly.com>
2023-03-19 13:07:22 -0500

    Added loading spinner

--- public/app.js
+++ public/app.js
@@ -1,43 +1,71 @@
-(() => {
-  const wrap = (selector, seconds) => {
-    document.getElementById(selector).addEventListener('click', () => {
-      const expiry = Date.now() / 1000 + seconds;
-      fetch('/set-expiry', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ expiry })
-      }).then(async (response) => { 
-        if (response.ok) setRemainingTime(expiry)
-      });
-    })
-  }
+let expiry;
 
-  const syncRemainingTime = async () => {
-    const response = await fetch('/get-expiry')
-    const ts = parseInt(await response.text());
-    setRemainingTime(ts)
-  }
+const isLocked = () => isNaN(expiry) || expiry < Date.now() / 1000;
+const setLoading = async (isLoading) => {
+  const overlay = document.getElementById('overlay');
+  return new Promise(resolve => {
+    overlay.style.display = 'flex';
+    setTimeout(() => {
+      if (isLoading) {
+        overlay.classList.remove('disabled');
+        setTimeout(() => { resolve() }, 200);
+      } else {
+        overlay.classList.add('disabled');
+        setTimeout(() => {
+          overlay.style.display = 'none';
+          resolve()
+        }, 1000);
+      }
+    }, 0);
+  });
+}
 
-  const setRemainingTime = (timestamp) => {
-    document.getElementById('remaining').innerText = makeTimeUntilString(timestamp)
-  }
+const fetchRemainingTime = async () => {
+  const response = await fetch('/get-expiry');
+  expiry = parseFloat(await response.text());
+}
 
-  const makeTimeUntilString = (timestamp) => {
-    const now = Date.now() / 1000;
-    if (isNaN(timestamp) || timestamp < now) return "The Retropie is Locked"
-    const diff = timestamp - now;
-    const minutes = Math.floor(diff / 60) % 60;
-    const hours = Math.floor(diff / 3600);
-    let message = hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''}, ` : '';
-    return `${message}${minutes} minute${minutes > 1 ? 's' : '' } left`;
-  }
+const wrapButton = (selector, seconds) => {
+  document.getElementById(selector).addEventListener('click', async () => {
+    const newExpiry = isLocked()
+      ? Date.now() / 1000 + seconds
+      : expiry + seconds;
+    await setLoading(true);
+    fetch('/set-expiry', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ expiry: newExpiry })
+    }).then(async (response) => {
+      if (response.ok) expiry = newExpiry;
+    }).finally(() => { setLoading(false); });
+  });
+}
 
-  document.addEventListener('DOMContentLoaded', () => {
-    wrap('zero', -100);
-    wrap('thirty', 30 * 60);
-    wrap('hour', 60 * 60);
-    wrap('two', 60 * 60 * 2);
-    syncRemainingTime();
-    setInterval(syncRemainingTime, 1000 * 10);
-  })
-})()
+const makeTimeUntilString = () => {
+  if (isLocked()) return "The Retropie is Locked"
+  const diff = expiry - Date.now() / 1000;
+  const minutes = Math.floor(diff / 60) % 60;
+  const hours = Math.floor(diff / 3600);
+  let message = hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''}, ` : '';
+  return `${message}${minutes} minute${minutes > 1 ? 's' : '' } left`;
+}
+
+const updateUIText = (id, text) => document.getElementById(id).innerText = text
+const updateUIElements = () => {
+  updateUIText('remaining', makeTimeUntilString());
+  updateUIText('thirty', isLocked() ? 'Allow for 30 minutes' : 'Add 30 minutes');
+  updateUIText('hour', isLocked() ? 'Allow for 1 hour' : 'Add 1 hour');
+  updateUIText('two', isLocked() ? 'Allow for 2 hours' : 'Add 2 hours');
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await fetchRemainingTime();
+  updateUIElements();
+  setInterval(updateUIElements, 500);
+  wrapButton('zero', -100 * 60);
+  wrapButton('thirty', 30 * 60);
+  wrapButton('hour', 60 * 60);
+  wrapButton('two', 60 * 60 * 2);
+  document.getElementById('content').classList.add('active');
+  await setLoading(false);
+});
